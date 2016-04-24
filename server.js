a17bc2073666b2b641c05b0cbbf1b8734d0b4818
Mohammed Rizvi <morizvi07@gmail.com>
2016-04-23 23:19:34 -0400

    Put index in views folder, script and css in public folder, set up server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require("express-session")
+var app = express();
+var PORT = 1738;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.listen(PORT, function() {
+  console.log('App listening on port %s', PORT);
+});
