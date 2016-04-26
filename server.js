c3d577deb3638d4ca4b0c49286740311e59854cf
Mohammed Rizvi <morizvi07@gmail.com>
2016-04-25 23:24:06 -0400

    added env.port for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var session = require("express-session")
 var app = express();
-var PORT = 1738;
+var PORT = process.env.PORT || 1738;
 
 app.use(express.static('public'));
 
